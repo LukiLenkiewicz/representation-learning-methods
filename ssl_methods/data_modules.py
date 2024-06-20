2deeae85eddb8cdcb4f39bd098bff5872286ab58
Lukasz Lenkiewicz <lukasz.lenkiewicz28@gmail.com>
2024-06-20 16:52:28 +0200

    fixes

--- ssl_methods/data_modules.py
+++ ssl_methods/data_modules.py
@@ -1,6 +1,7 @@
-from torchvision.datasets import STL10
+import torch
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
+from torchvision.datasets import STL10
 
 import pytorch_lightning as pl
 
@@ -28,21 +29,19 @@ class FinetuningDataModule(pl.LightningDataModule):
 
 
 class ReconstructionDataModule(pl.LightningDataModule):
-    def __init__(self, path, preprocess, batch_size=32):
+    def __init__(self, preprocess, path="./data", batch_size=32):
         super().__init__()
         self.path = path
         self.batch_size = batch_size
         self.preprocess = preprocess
 
     def setup(self, stage=None):
-        dataset = STL10(self.path, split="unlabeled", transform=self.preprocess)
-        self.train_dataset, self.val_dataset, self.test_dataset = random_split(dataset, [.9, .1, .1])
+        generator = torch.Generator().manual_seed(42)
+        self.dataset = STL10(self.path, split="unlabeled", transform=self.preprocess)
+        self.train_dataset, self.val_dataset = random_split(self.dataset, [0.8, 0.2], generator=generator)
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.batch_size)
-
-    def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size)
