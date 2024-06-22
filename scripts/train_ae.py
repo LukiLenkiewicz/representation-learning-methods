810cf213a5daa83e71f81da989bb87f288843367
Lukasz Lenkiewicz <lukasz.lenkiewicz28@gmail.com>
2024-06-22 12:57:06 +0200

    model checkpoints add

--- scripts/train_ae.py
+++ scripts/train_ae.py
@@ -1,13 +1,14 @@
 import torch
 import torch.nn as nn
 from torchvision import transforms
+
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 from ssl_methods.encoder import ResNet18Encoder
 from ssl_methods.decoder import ResNet18Decoder
 from ssl_methods.data_modules import ReconstructionDataModule
 from ssl_methods.train_modules import BasePretrainingModule
-
 
 def main():
     preprocess = transforms.Compose(
@@ -24,7 +25,8 @@ def main():
     data_module.setup()
     training_module = BasePretrainingModule(model)
 
-    trainer = pl.Trainer(max_epochs=50)
+    checkpoint_callback = ModelCheckpoint(dirpath="./models", monitor="val_loss")
+    trainer = pl.Trainer(max_epochs=50, callbacks=[checkpoint_callback])
     trainer.fit(training_module, data_module)
 
 
--- scripts/train_flow.py
+++ scripts/train_flow.py
@@ -1,5 +1,6 @@
 from torchvision import transforms
 import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 from ssl_methods.data_modules import ReconstructionDataModule
 from ssl_methods.latent_flow.train_module import LatentFlowPretrainingModule
@@ -16,7 +17,8 @@ def main():
     data_module.setup()
     training_module = LatentFlowPretrainingModule()
 
-    trainer = pl.Trainer(max_epochs=50)
+    checkpoint_callback = ModelCheckpoint(dirpath="./models", monitor="val_loss")
+    trainer = pl.Trainer(max_epochs=50, callbacks=[checkpoint_callback])
     trainer.fit(training_module, data_module)
 
 
